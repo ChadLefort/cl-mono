febcc59cba201f3f3782c653fd43ac1ccd3ba207
Chad Lefort <chadlefort@gmail.com>
2024-05-29 12:14:33 -0500

    Added IntersectionObserver stub

--- apps/portfolio/vitest-setup.ts
+++ apps/portfolio/vitest-setup.ts
@@ -15,6 +15,12 @@ const createMatchMedia = (width: number) => {
   });
 };
 
-beforeAll(() => {
-  window.matchMedia = createMatchMedia(window.innerWidth);
-});
+const IntersectionObserverMock = vi.fn(() => ({
+  disconnect: vi.fn(),
+  observe: vi.fn(),
+  takeRecords: vi.fn(),
+  unobserve: vi.fn(),
+}));
+
+vi.stubGlobal('IntersectionObserver', IntersectionObserverMock);
+vi.stubGlobal('matchMedia', createMatchMedia(window.innerWidth));
